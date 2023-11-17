6b42f6b2e42192267392c68a98e08866ef0d0d20
agusbw <agusbudhiarta81@gmail.com>
2023-11-17 19:21:07 +0800

    chore: change loading simulation delay

--- components/player-list.tsx
+++ components/player-list.tsx
@@ -5,7 +5,7 @@ async function getPlayers(): Promise<Player[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/players`);
 
   //delay untuk simulasi loading saja
-  await new Promise((resolve) => setTimeout(resolve, 1500));
+  await new Promise((resolve) => setTimeout(resolve, 800));
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
@@ -26,7 +26,7 @@ export default async function PlayerList({
     players = players.filter((player) => player.position === selectedPosition);
   }
   return (
-    <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3 mt-4">
+    <div className="grid grid-cols-1 gap-4 mt-4 lg:grid-cols-2 xl:grid-cols-3">
       {players.map((player) => (
         <PlayerCard
           key={player.username}
